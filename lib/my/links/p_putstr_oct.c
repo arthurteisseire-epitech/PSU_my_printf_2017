8c21b29ab271523ac93162307ff687d0f5b0e017
Arthur <arthur.teisseire@epitech.eu>
2017-11-15 10:24:30 +0100

    the size of %S wasn't handled: now it works

--- lib/my/links/p_putstr_oct.c
+++ lib/my/links/p_putstr_oct.c
@@ -19,15 +19,18 @@ int p_putstr_oct(va_list ap)
 	char *str = va_arg(ap, char *);
 	char *base = "01234567";
 	int i = 0;
+	int size = 0;
 
 	while (str[i] != '\0') {
-		if (is_printable(str[i]))
+		if (is_printable(str[i])) {
 			my_putchar(str[i]);
-		else {
+			size++;
+		} else {
 			my_putchar('\\');
-			my_putnbr_base(str[i], base);
+			size++;
+			size += my_putnbr_base(str[i], base);
 		}
 		i++;
 	}
-	return (0);
+	return (size);
 }
